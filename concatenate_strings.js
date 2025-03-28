8df3ec98369a96deaa9ac72468bcb0b1a5894d9f
JuanDavidRojasBurbano <juan.david.rojas.burbano0@gmail.com>
2025-03-28 13:29:30 -0500

    Subiendo ejercicio del video sobre concatenar cadenas

---
+++ concatenate_strings.js
@@ -0,0 +1,11 @@
+var myNoun = "perro"; 
+var myAdjective = "negro"; 
+var myVerb = "corrió";
+var myAdverb = "rápidamente";
+
+/* Concatena las cadenas para crear una cadena que muestre un mensaje. Puedes cambiar los valores de las variables.
+Por ejemplo: El perro negro corrió rápidamente a la tienda.
+*/
+
+var blankWords = "El " + myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb + " a la tienda" ;
+console.log(blankWords);
